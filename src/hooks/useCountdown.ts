e6f182c40ae6affea01dcc84bd41f9c85d8c2852
yosef lakew <80267669+yoseflakew25@users.noreply.github.com>
2023-08-31 11:48:04 +0300

    .

--- src/hooks/useCountdown.ts
+++ src/hooks/useCountdown.ts
@@ -1,7 +1,7 @@
-import { useEffect, useState, useRef, useCallback } from 'react';
+import { useEffect, useState, useRef, useCallback } from "react";
 
-export const useCountdown = (initialValue: number, interval = 1000) => {
-  const intervalRef = useRef<NodeJS.Timer | null>(null);
+export const useCountdown = (initialValue: number, interval?: number) => {
+  const intervalRef = useRef<ReturnType<typeof setInterval> | null>(null);
   const [countdown, setCountdown] = useState(initialValue);
 
   const startCountdown = useCallback(() => {
@@ -9,23 +9,25 @@ export const useCountdown = (initialValue: number, interval = 1000) => {
     intervalRef.current = setInterval(() => {
       setCountdown((prev) => {
         if (prev > 0) {
-          return prev - interval;
+          return prev - (interval || 1000);
         }
-        if (prev === 0) clearInterval(intervalRef.current!);
+        if (prev === 0)
+          clearInterval(intervalRef.current as ReturnType<typeof setInterval>);
 
         return prev;
       });
-    }, interval);
-  }, [initialValue]);
+    }, interval || 1000);
+  }, [initialValue, interval]);
 
   const resetCountdown = useCallback(() => {
-    clearInterval(intervalRef.current!);
+    clearInterval(intervalRef.current as ReturnType<typeof setInterval>);
     intervalRef.current = null;
     setCountdown(initialValue);
   }, [initialValue]);
 
   useEffect(() => {
-    return () => clearInterval(intervalRef.current!);
+    return () =>
+      clearInterval(intervalRef.current as ReturnType<typeof setInterval>);
   }, [interval]);
 
   return { countdown, startCountdown, resetCountdown };
