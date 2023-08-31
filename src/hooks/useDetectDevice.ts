e6cc25958cf3603ec6bdf709d706fe5c162fa31f
yosef lakew <80267669+yoseflakew25@users.noreply.github.com>
2023-08-31 17:03:58 +0300

    .

--- src/hooks/useDetectDevice.ts
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react';
-
-import { isMobile } from '../utils';
-
-export const useDetectDevice = () => {
-  const [isMobileDevice] = useState(() => isMobile());
-
-  return isMobileDevice;
-};
